e6f38dc1bd306c56e703e0564fc6cb859516ff7a
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2024-12-20 12:57:21 +0530

    individual model changed

--- models/Individual.js
+++ models/Individual.js
@@ -8,6 +8,8 @@ const IndividualSchema = new mongoose.Schema({
   postcode: { type: String, required: true },
   password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  otp: { type: String }, // Add OTP field
+  otpExpire: { type: Date } // Add OTP expiration field
 });
 
 export default mongoose.model('Individual', IndividualSchema);
