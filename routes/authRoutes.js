1bafaaa5efe189c50db11bab49ba68419975b194
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2024-12-31 18:22:23 +0530

    storepackage added in authroute

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,16 @@
 import express from 'express';
-import { registerUser, loginUser,getLoggedInUser, logoutUser,requestOtp,resetPassword } from '../controllers/authController.js';
+import { registerUser , loginUser , getLoggedInUser , logoutUser , requestOtp, resetPassword, storeSelectedPackage } from '../controllers/authController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
-import { getSessionData } from '../controllers/authController.js';
+
 const router = express.Router();
 
 // Routes
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/me', getLoggedInUser);
-router.post('/logout', logoutUser);
-router.get('/session', getSessionData);
+router.post('/register', registerUser );
+router.post('/login', loginUser );
+router.get('/me', getLoggedInUser );
+router.post('/logout', logoutUser );
 router.post('/request-otp', requestOtp);
 router.post('/reset-password', resetPassword);
-
+router.post('/store-package', storeSelectedPackage); // Add this line
 
 export default router;
-
