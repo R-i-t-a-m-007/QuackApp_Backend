a4f0a8e8f1ad2b5419df80775d0213002203d72c
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-01-22 16:14:40 +0530

    session updated

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -1,20 +1,17 @@
 export const sessionMiddleware = (req, res, next) => {
-  // Check if any of the session types are present
   const { user, company, worker } = req.session;
 
   if (!user && !company && !worker) {
     return res.status(401).json({ message: 'Unauthorized. Please log in.' });
   }
 
-  // Optionally, you can log the type of session that is active
-  // if (user) {
-  //   console.log('User  session active:', user);
-  // } else if (company) {
-  //   console.log('Company session active:', company);
-  // } else if (worker) {
-  //   console.log('Worker session active:', worker);
-  // }
+  if (user) {
+    console.log('User  session active:', user);
+  } else if (company) {
+    console.log('Company session active:', company);
+  } else if (worker) {
+    console.log('Worker session active:', worker);
+  }
 
-  // Proceed to the next middleware or route handler
   next();
 };
