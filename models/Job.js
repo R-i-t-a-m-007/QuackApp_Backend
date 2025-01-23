e7e7796390c250818f45f6eb25d5555bffc99d1f
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-01-23 16:42:05 +0530

    job model changed

--- models/Job.js
+++ models/Job.js
@@ -8,7 +8,7 @@ const jobSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   shift: { type: String, required: true },
   workersRequired: { type: Number, required: true },
-  workers: { type: [mongoose.Schema.Types.ObjectId], ref: 'Worker', default: null }, // Array of workers
+  workers: { type: [mongoose.Schema.Types.ObjectId], ref: 'Worker', default: [] }, // Array of workers
   jobStatus: { type: Boolean, default: false }, // Job status
   userCode: { type: String, required: true }, // User code of the creator
   createdAt: { type: Date, default: Date.now },
