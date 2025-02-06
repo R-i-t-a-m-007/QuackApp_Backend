4f1bff02605c656ca6b979c8a80f5d4a95149893
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-02-06 18:14:35 +0530

    logs removed in session

--- middlewares/sessionMiddleware.js
+++ middlewares/sessionMiddleware.js
@@ -2,7 +2,6 @@ export const sessionMiddleware = (req, res, next) => {
   // Check if any of the session types are present
   const { user, company, worker } = req.session;
 
-  console.log('Session Data:', req.session); // Log the session data
 
   if (!user && !company && !worker) {
     return res.status(401).json({ message: 'Unauthorized. Please log in.' });
