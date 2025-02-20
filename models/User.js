72661c0acdcd92560c9989a33578dea1e50f7892
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-02-20 16:55:33 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -10,13 +10,14 @@ const UserSchema = new mongoose.Schema({
   package: { 
     type: String, 
     enum: ['Basic', 'Pro'], 
-    default: null, // Allow null as a default value
+    default: null,
   },
+  price: { type: Number, default: 0 }, // New price field
   createdAt: { type: Date, default: Date.now },
-  otp: { type: String }, // Add OTP field
-  otpExpire: { type: Date }, // Add OTP expiration field
-  image: { type: String, default: null }, // Field to store the user's profile image
-  userCode: { type: String, required: true }, // New field for user code
+  otp: { type: String },
+  otpExpire: { type: Date },
+  image: { type: String, default: null },
+  userCode: { type: String, required: true },
 });
 
 export default mongoose.model('User ', UserSchema);
