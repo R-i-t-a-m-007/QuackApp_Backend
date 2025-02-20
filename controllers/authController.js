75be023e7c17e140dfd220ed61142ce13ba052bf
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-02-20 11:37:13 +0530

    fetch user by id

--- controllers/authController.js
+++ controllers/authController.js
@@ -383,3 +383,15 @@ export const deleteUser = async (req, res) => {
     res.status(500).json({ message: 'Failed to delete user.' });
   }
 };
+
+export const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser , loginUser , getLoggedInUser , logoutUser , requestOtp, resetPassword, storeSelectedPackage,getSessionData,updateUserPackage, uploadUserImage, getAllUsers, updateUserDetails, deleteUser } from '../controllers/authController.js';
+import { registerUser , loginUser , getLoggedInUser , logoutUser , requestOtp, resetPassword, storeSelectedPackage,getSessionData,updateUserPackage, uploadUserImage, getAllUsers, updateUserDetails, deleteUser, getUserById } from '../controllers/authController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -18,6 +18,7 @@ router.post('/:userId/upload-image', uploadUserImage); // New route for uploadin
 router.get('/users', getAllUsers); // API to get all users
 router.put('/update/:userId', updateUserDetails);
 router.delete('/users/:userId', deleteUser);
+router.get("/users/:id",getUserById);
 
 
 
