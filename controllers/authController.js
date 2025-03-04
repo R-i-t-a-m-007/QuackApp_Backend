cc591b9522a3dc892477448184588aaa4e92e77e
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-03-04 18:52:45 +0530

    user controller getalluser updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -354,10 +354,6 @@ export const getAllUsers = async (req, res) => {
   try {
     const users = await User.find().select('-password -otp -otpExpire'); // Exclude sensitive fields
 
-    if (!users || users.length === 0) {
-      return res.status(404).json({ message: 'No users found' });
-    }
-
     res.status(200).json(users);
   } catch (error) {
     console.error('Error fetching users:', error);
