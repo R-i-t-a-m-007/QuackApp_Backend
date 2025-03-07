0d77c4f1ff9f11e141fb6605ba6b87d206162787
R-i-t-a-m-007 <ritammahanty6@gmail.com>
2025-03-07 19:13:48 +0530

    origin added in admin

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(
 // Configure CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://quackapp-admin.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // Allow cookies
   })
